ab08e81a3ddd5291e4666030f7a9e72f13f85507
Mitanshu Patel <Mitanshu.Patel@civica.com>
2025-12-07 22:17:42 +0530

    sln file change

--- NexOrder.AuthService.Infrastructure/obj/Debug/net8.0/NexOrder.AuthService.Infrastructure.AssemblyInfo.cs
+++ NexOrder.AuthService.Infrastructure/obj/Debug/net8.0/NexOrder.AuthService.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexOrder.AuthService.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+598fcca28dffed432cad3ac50111344c6068416d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexOrder.AuthService.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexOrder.AuthService.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
